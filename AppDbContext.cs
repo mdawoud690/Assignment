823e3fae6d5f4c77060bd4eb35f9beb7f56f1584
mdawoud690 <132380466+mdawoud690@users.noreply.github.com>
2023-11-25 00:32:15 +0300

    Add files via upload

---
+++ AppDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Nextwo.Bl.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nextwo.Ef
+{
+    public class AppDbContext:IdentityDbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+
+        }
+        public DbSet<Employee> Employees { get; set; }
+    }
+}
